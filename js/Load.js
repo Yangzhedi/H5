5238efd6a883d9f0fb41c144b29f63d5b8a8008a
Yangzhedi <uiryzd@163.com>
2016-08-29 13:13:34 +0800

    load & ring

---
+++ js/Load.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Administrator on 2016/8/29.
+ */
+var res = [
+    'Base','Bar','Bar_v','Point','Polyline',
+    'Radar','Pie','Ring'
+];
+var html = [];
+for(s in res){
+    html.push('<script type="text/javascript" src="js/H5Component'+res[s]+'.js"><\/script>');
+    html.push('<link rel="stylesheet" type="text/css" href="css/H5Component'+res[s]+'.css">');
+}
+document.write( html.join('') );
