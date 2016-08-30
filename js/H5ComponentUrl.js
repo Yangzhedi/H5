b4852226489dec7e35eab0bc9d512fba20d80198
Yangzhedi <uiryzd@163.com>
2016-08-30 09:57:37 +0800

    url

--- js/H5ComponentUrl.js
+++ js/H5ComponentUrl.js
@@ -1,19 +1,21 @@
 /*基本图文组件对象*/
-var H5ComponentBase = function( name, cfg ){
+var H5ComponentUrl = function( name, cfg ){
 	var cfg = cfg || {};
 	//给div加id
 	var id = ( 'h5_c_' + Math.random()).replace(".","_") ;
 	//把当前的组件类型添加到样式中进行标记
 	var cls = 'h5_component_' + cfg.type  ;
-	var component = $('<div class="h5_component '+cls+' h5_component_name_'+name+'" id="'+id+'">');
+	var component = $('<a href="http://www.baidu.com" class="h5_component '+cls+' h5_component_name_'+name+'" id="'+id+'">');
 
-	cfg.text   && component.text(cfg.text);
-	cfg.width  && component.width(cfg.width/2);
-	cfg.height && component.height(cfg.height/2);
+	cfg.text && component.text(cfg.text);
 
     cfg.css && component.css(cfg.css);
-    cfg.bg && component.css('backgroundImage','url('+ cfg.bg +')');
 
+    if( cfg.decoration === false){
+        component.css({
+            textDecoration : 'none'
+        })
+    }
     //通过居中参数 设置居中
     if( cfg.center === true){
         component.css({
@@ -24,21 +26,7 @@ var H5ComponentBase = function( name, cfg ){
         })
     }
     //  ...很多自定义的参数
-    /*var cfg1 = {
-        type : 'url',
-        text : '文本内容',
-        /!*css:{
-            left:20,
-            bottom:30,
-        },*!/
-        decoration:false
-    };*/
-    if(cfg.url){
-        var h5 = new H5ComponentUrl('myUrl',cfg.url);
-    }
 
-
-    component.append(h5);
     component.on('onLoad',function(){
         component.addClass(cls+'_load').removeClass(cls+'_leave');
         //动画
--- js/README.md
+++ js/README.md
@@ -1,2 +1,17 @@
 # H5Component
-js
+### Base.js     :最基本的图文组件
+可设置背景，文字，blablabla。
+### Bar.js      :柱状图组件
+可设置每条柱的颜色
+### Bar_v.js    :竖版柱状图组件
+### Point.js    :散点图组件
+### Polyline.js :折线图组件
+### Radar.js    :雷达图组件
+### Pie.js      :饼图组件
+### Ring.js     :环图组件
+`均可通过  var H5 = new H5ComponentXXX(name,cfg); 调用`
+***
+以上都是通过imooc网实战课程习得
+以下是个人复用拓展
+***
+### Url.js :链接组件
